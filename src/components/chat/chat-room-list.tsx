69bc11012094690e6f6a9df8557ec243bc8d9f6e
JongMany <blackberry1114@naver.com>
2024-10-13 18:24:38 +0900

    feat: 서버 컴포넌트에서 호출하도록 수정

--- src/components/chat/chat-room-list.tsx
+++ src/components/chat/chat-room-list.tsx
@@ -1,40 +1,35 @@
-'use client';
-
-import { ChatRoomDetail, ChatRoomDto } from '@/models/chat/chat-room.dto';
-import { getSession } from 'next-auth/react';
+import { auth } from '@/auth';
+import { ChatRoomDto } from '@/models/chat/chat-room.dto';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 
-export const ChatRoomList = () => {
-  const [chatRooms, setChatRooms] = useState<ChatRoomDetail[]>([]);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const session = await getSession();
-
-        const response = await fetch('http://localhost:8080/api/gpt/threads', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${session?.user?.accessToken}`,
-          },
-        });
-        const data: ChatRoomDto = await response.json();
-        if (data.success) {
-          setChatRooms(data.threads);
-        }
-      } catch (error) {
-        console.error(error);
-      }
+export const ChatRoomList = async () => {
+  async function fetchData() {
+    try {
+      const session = await auth();
+      const response = await fetch('http://localhost:8080/api/gpt/threads', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${session?.user?.accessToken}`,
+        },
+      });
+      const data: ChatRoomDto = await response.json();
+      return data.threads;
+    } catch (error) {
+      console.error(error);
     }
-    fetchData();
-  }, []);
+  }
+  const chatRooms = (await fetchData()) || [];
 
   return (
-    <main>
-      {chatRooms.map((room) => (
-        <Link key={room.threadId} href={`/chat/${room.threadId}`}>
-          <div>
+    <main className="grid grid-cols-2 px-2 py-2 bg-white gap-x-10">
+      {chatRooms?.map((room) => (
+        <Link
+          key={room.threadId}
+          href={`/chat/${room.threadId}`}
+          className="rounded-md border border-gray-200"
+        >
+          <div className="h-[60px] flex items-center justify-center">
             <p>{room.type}</p>
           </div>
         </Link>
