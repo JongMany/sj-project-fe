beb5b941d208fccd1e1e5246bf72a01643f39ba5
JongMany <blackberry1114@naver.com>
2024-10-18 17:55:19 +0900

    feat: 기본맨 기억 수정 링크 구현

--- src/app/(after-auth)/chat/(introduction)/general/page.tsx
+++ src/app/(after-auth)/chat/(introduction)/general/page.tsx
@@ -1,7 +1,12 @@
+import { auth } from '@/auth';
 import { CreateChatButton } from '@/components/chat/create-chat-button';
+import { userType } from '@/constants/user/user-type';
+import Link from 'next/link';
 import React from 'react';
 
-function Page() {
+async function Page() {
+  const session = await auth();
+
   return (
     <div className="bg-white px-8 py-6">
       <section className="mb-8 flex justify-center">
@@ -9,7 +14,13 @@ function Page() {
       </section>
       <section className="bg-gray-100 px-5 py-4 text-black text-[16px] rounded-md">
         <h4 className="mb-5 font-semibold">기본맨</h4>
-        <p>AI Assistant와 대화를 나눠보세요</p>
+        <p>
+          {session?.user?.group &&
+            userType[session?.user?.group].removeMemory && (
+              <Link href="/setting">GPT 기억 수정하기</Link>
+            )}
+          <span>AI Assistant와 대화를 나눠보세요</span>
+        </p>
       </section>
     </div>
   );
