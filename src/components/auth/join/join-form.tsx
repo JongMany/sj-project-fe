94652474b5cfcc709c0575e1a2e4c1c88d3e42a3
JongMany <blackberry1114@naver.com>
2024-10-21 14:03:32 +0900

    feat: 환경변수 주입

--- src/components/auth/join/join-form.tsx
+++ src/components/auth/join/join-form.tsx
@@ -32,7 +32,7 @@ export default function JoinForm() {
     // Register Fetch
 
 
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/register`, {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/auth/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
