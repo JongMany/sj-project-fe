a3a66668ccd974490aa11e7398f14cbad918f8e1
JongMany <blackberry1114@naver.com>
2024-10-24 23:53:02 +0900

    feat: edit memory function

--- src/components/memory/memory-list.tsx
+++ src/components/memory/memory-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {MdDelete, MdModeEdit} from "react-icons/md";
 import {MemoryType} from "@/app/(after-auth)/chat/setting/[id]/page";
 import {Modal} from "antd";
@@ -30,14 +30,15 @@ function MemoryList({
   const [memoryList, setMemoryList] = useState(memories);
   const [isShowModal, setIsShowModal] = useState(false);
   const session = useSession();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const closeModal = () => {
     setIsShowModal(false);
   }
 
   const deleteMemory = async (memoryId: string) => {
-    try{
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL||''}/api/v1/memory/${memoryId}`, {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/memory/${memoryId}`, {
         method: 'Delete',
         headers: {
           'Content-Type': 'application/json',
@@ -45,42 +46,78 @@ function MemoryList({
         },
         credentials: 'include',
       });
-      if(!response.ok) {
+      if (!response.ok) {
         throw new Error("error");
       }
       const data = await response.json();
-      console.log(data);
-      if(data.success) {
+      if (data.success) {
         setMemoryList(data.memories);
       }
     } catch (error) {
       console.error("error", error);
-      showToast("error", "삭제가 실패했습니다.");
+      showToast("error", "삭제에 실패했습니다.");
       setMemoryList(memoryList);
     }
-
   }
 
-  const showEditConfirm = () => {
+  const editMemory = async (memoryId: string, changeValue: string)=> {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/memory/${memoryId}`, {
+        method: 'Put',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${session.data?.user?.accessToken}`,
+        },
+        body: JSON.stringify({
+          description: changeValue,
+        }),
+        credentials: 'include',
+      });
+      if (!response.ok) {
+        throw new Error("error");
+      }
+      const data = await response.json();
+      if (data.success) {
+        setMemoryList(prev => prev.map((item) => item.id !== memoryId ? item : {
+          ...item,
+          description: changeValue,
+        }));
+      }
+    } catch (error) {
+      console.error("error", error);
+      showToast("error", "수정에 실패했습니다.");
+      setMemoryList(memoryList);
+    }
+  }
+
+
+  const showEditConfirm = (memory: MemoryType) => () => {
     confirm({
       title: "수정하시겠습니까?",
       content: <>
-
+        <div>
+            <label htmlFor="description"/>
+            <input id="description" className="bg-white text-black border-2 px-2 py-1 rounded-md"
+                   defaultValue={memory.description} ref={inputRef} />
+        </div>
       </>,
-      onOk: ()=>{console.log("OK")},
+      onOk: () => {
+
+        console.log("OK", inputRef.current.value);
+      },
       onCancel: closeModal
     });
   }
 
   const showDeleteConfirm = (memoryId: string) => () => {
+
     confirm({
       title: "삭제하시겠습니까?",
       content: <>
         삭제한 후에는 변경할 수 없습니다.
       </>,
-      onOk: async ()=>{
-        const memories = await deleteMemory(memoryId);
-        // setMemoryList(memories);
+      onOk: async () => {
+        await deleteMemory(memoryId);
       },
       onCancel: closeModal
     });
@@ -89,13 +126,13 @@ function MemoryList({
   return (
       <>
         <div className="overflow-scroll h-[80dvh] bg-gray-100 rounded-md text-black flex-1 px-4 py-4">
-          {memoryList.map((memory) => <div key={memory.id} className="flex items-center justify-between">
+          {memoryList.map((memory) => <div key={memory.id} className="flex items-center justify-between mb-3">
             <div className="flex gap-x-2 items-center">
-              <span className="w-[50px] font-semibold text-[16px]">{title[memory.type]}</span>
+              <span className="w-[80px] font-semibold text-[16px]">{title[memory.type]}</span>
               <span>{memory.description}</span>
             </div>
-            <div className="flex text-[16px] gap-x-1 items-center">
-            <span className="text-gray-500 cursor-pointer" onClick={showEditConfirm}>
+            <div className="flex text-[15px] gap-x-1 items-center">
+            <span className="text-gray-500 cursor-pointer" onClick={showEditConfirm(memory)}>
               <MdModeEdit/>
             </span>
               <span className="text-gray-500 cursor-pointer" onClick={showDeleteConfirm(memory.id)}>
