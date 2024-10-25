28b7d96319c50b899ec0e10810e6b8992d38f207
JongMany <blackberry1114@naver.com>
2024-10-25 10:08:59 +0900

    feat: memory list ui 수정

--- src/components/memory/memory-list.tsx
+++ src/components/memory/memory-list.tsx
@@ -60,7 +60,7 @@ function MemoryList({
     }
   }
 
-  const editMemory = async (memoryId: string, changeValue: string)=> {
+  const editMemory = async (memoryId: string, changeValue: string) => {
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/memory/${memoryId}`, {
         method: 'Put',
@@ -73,15 +73,20 @@ function MemoryList({
         }),
         credentials: 'include',
       });
+
       if (!response.ok) {
         throw new Error("error");
       }
       const data = await response.json();
+      console.log(response, data);
       if (data.success) {
-        setMemoryList(prev => prev.map((item) => item.id !== memoryId ? item : {
+        const {id, description} = data;
+        setMemoryList(prev => prev.map((item) => item.id !== id ? item : {
           ...item,
-          description: changeValue,
+          description,
         }));
+      } else {
+        showToast("error", "수정에 실패했습니다.");
       }
     } catch (error) {
       console.error("error", error);
@@ -96,14 +101,14 @@ function MemoryList({
       title: "수정하시겠습니까?",
       content: <>
         <div>
-            <label htmlFor="description"/>
-            <input id="description" className="bg-white text-black border-2 px-2 py-1 rounded-md"
-                   defaultValue={memory.description} ref={inputRef} />
+          <label htmlFor="description"/>
+          <input id="description" className="bg-white text-black border-2 px-2 py-1 rounded-md"
+                 defaultValue={memory.description} ref={inputRef}/>
         </div>
       </>,
-      onOk: () => {
-
-        console.log("OK", inputRef.current.value);
+      onOk: async () => {
+        await editMemory(memory.id, inputRef.current.value);
+        // console.log("OK", inputRef.current.value);
       },
       onCancel: closeModal
     });
@@ -126,10 +131,10 @@ function MemoryList({
   return (
       <>
         <div className="overflow-scroll h-[80dvh] bg-gray-100 rounded-md text-black flex-1 px-4 py-4">
-          {memoryList.map((memory) => <div key={memory.id} className="flex items-center justify-between mb-3">
+          {memoryList.map((memory) => <div key={memory.id} className="flex items-center justify-between mb-4 gap-x-2">
             <div className="flex gap-x-2 items-center">
-              <span className="w-[80px] font-semibold text-[16px]">{title[memory.type]}</span>
-              <span>{memory.description}</span>
+              <span className="min-w-[70px] font-semibold text-[14px]">{title[memory.type]}</span>
+              <span className="text-[12px]">{memory.description}</span>
             </div>
             <div className="flex text-[15px] gap-x-1 items-center">
             <span className="text-gray-500 cursor-pointer" onClick={showEditConfirm(memory)}>
