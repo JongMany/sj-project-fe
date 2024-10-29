01b45765141cb75dcfeee0badc6521532cc611cf
JongMany <blackberry1114@naver.com>
2024-10-29 21:55:43 +0900

    feat: 친절맨, 피드백맨 설명 추가

--- src/app/(after-auth)/chat/(introduction)/kind/page.tsx
+++ src/app/(after-auth)/chat/(introduction)/kind/page.tsx
@@ -1,15 +1,15 @@
-import { CreateChatButton } from '@/components/chat/create-chat-button';
+import {CreateChatButton} from '@/components/chat/create-chat-button';
 import React from 'react';
 import {ChatTypeIntroduction} from "@/components/chat/chat-type-introduction";
 
 function Page() {
   return (
-    <div className="bg-white px-8 py-6">
-      <section className="mb-8 flex justify-center">
-        <CreateChatButton type="Kind" />
-      </section>
-      <ChatTypeIntroduction type="Kind" introduction={<p>설명 추가 예정</p>}/>
-    </div>
+      <div className="bg-white px-8 py-6">
+        <section className="mb-8 flex justify-center">
+          <CreateChatButton type="Kind"/>
+        </section>
+        <ChatTypeIntroduction type="Kind" introduction={<p>대화를 나누며 기분을 안정시키고 싶다면 대화를 시작해보세요</p>}/>
+      </div>
   );
 }
 
