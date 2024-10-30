44e06d295dbeede4bfc2c224fc2d1b32fd2cae9c
JongMany <blackberry1114@naver.com>
2024-10-30 23:08:05 +0900

    feat: setting/[id]/page 접근 시 memory-view 증가

--- src/app/(after-auth)/chat/setting/[id]/page.tsx
+++ src/app/(after-auth)/chat/setting/[id]/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { decodeByAES256 } from '@/utils/encode';
-import { CHATROOM_TYPE } from '@/constants/chat/room-type';
-import { AssistantType } from '@/models/chat/chat-room.dto';
-import { auth } from '@/auth';
+import {decodeByAES256} from '@/utils/encode';
+import {CHATROOM_TYPE} from '@/constants/chat/room-type';
+import {AssistantType} from '@/models/chat/chat-room.dto';
+import {auth} from '@/auth';
 import MemoryList from '@/components/memory/memory-list';
-import { FaBrain } from 'react-icons/fa6';
+import {FaBrain} from 'react-icons/fa6';
 import {MemoryType} from "@/models/memory/memory.model";
 
 type Props = {
@@ -14,9 +14,20 @@ type Props = {
   searchParams: { type: AssistantType };
 };
 
-async function Page({ params: { id }, searchParams: { type } }: Props) {
+async function Page({params: {id}, searchParams: {type}}: Props) {
   const threadId = decodeByAES256(id);
   const session = await auth();
+
+  await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/event/memory-view`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.user?.accessToken}`,
+    },
+    body: JSON.stringify({
+      type
+    })
+  })
 
   const memories: MemoryType[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/memory/${threadId}`, {
     method: 'GET',
@@ -40,15 +51,15 @@ async function Page({ params: { id }, searchParams: { type } }: Props) {
   });
 
   return (
-    <div className="bg-white px-4 py-4 h-[calc(100dvh-50px)]">
-      <div className="h-full flex flex-col justify-between gap-8">
-        <h3 className="text-black font-semibold text-[18px] flex items-center gap-x-3">
-          <span>{CHATROOM_TYPE[type]}이 알고 있는 {session?.user?.name} 님의 정보</span>
-          <FaBrain />
-        </h3>
-        <MemoryList memories={memories} />
+      <div className="bg-white px-4 py-4 h-[calc(100dvh-50px)]">
+        <div className="h-full flex flex-col justify-between gap-8">
+          <h3 className="text-black font-semibold text-[18px] flex items-center gap-x-3">
+            <span>{CHATROOM_TYPE[type]}이 알고 있는 {session?.user?.name} 님의 정보</span>
+            <FaBrain/>
+          </h3>
+          <MemoryList memories={memories}/>
+        </div>
       </div>
-    </div>
   );
 }
 
