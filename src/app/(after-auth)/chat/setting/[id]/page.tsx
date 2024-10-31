ed4475863cf17d67fb197273804647bfe2aa85ed
JongMany <blackberry1114@naver.com>
2024-10-31 18:59:28 +0900

    chore: 할일 주석처리

--- src/app/(after-auth)/chat/setting/[id]/page.tsx
+++ src/app/(after-auth)/chat/setting/[id]/page.tsx
@@ -18,6 +18,7 @@ async function Page({params: {id}, searchParams: {type}}: Props) {
   const threadId = decodeByAES256(id);
   const session = await auth();
 
+  // TODO: 수정!
   await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/event/memory-view`, {
     method: 'POST',
     headers: {
