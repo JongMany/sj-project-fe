77c9191d1cd184bc994ca0f55785e5847914eb09
JongMany <blackberry1114@naver.com>
2024-11-10 14:13:57 +0900

    feat: 어드민 유저 페이지 디자인

--- src/app/admin/user/page.tsx
+++ src/app/admin/user/page.tsx
@@ -19,21 +19,26 @@ async function Page({ searchParams: { id1 } }: Props) {
   const data = await response.json();
 
   return (
-    <div>
-      <div>
-        <div>User 정보</div>
-        <div>ID: {data.id}</div>
+    <div className="px-4 py-8 text-black">
+      <div className="flex flex-col gap-y-2 mb-4">
+        <div className="text-black text-[18px] font-bold">User 정보</div>
+        <div className="flex">
+          ID: {data.id} / {data.email.split('@')[0]}
+        </div>
         <div>Group: {data.group}</div>
       </div>
-      <div>
-        <div>사용 중인 스레드</div>
+      <div className="flex flex-col gap-y-2">
+        <div className="text-[18px] font-bold">사용 중인 스레드</div>
         <div className="flex flex-col">
           {data?.threads?.length > 0 ? (
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             data?.threads?.map((thread: any) => (
               <div key={thread.threadId} className="flex gap-x-3">
                 <div className={'min-w-[80px]'}>{thread.type}</div>
-                <Link href={`/admin/${encodedId}/thread?id=${thread.threadId}`}>
+                <Link
+                  href={`/admin/${encodedId}/thread?id=${thread.threadId}`}
+                  className="text-gray-500"
+                >
                   대화 내용 보기
                 </Link>
               </div>
@@ -42,7 +47,10 @@ async function Page({ searchParams: { id1 } }: Props) {
             <div>사용 중인 스레드가 없습니다</div>
           )}
         </div>
-        <Link href={`/admin/${encodedId}/logs?id=${encodedId}`}>
+        <Link
+          href={`/admin/${encodedId}/logs?id=${encodedId}`}
+          className="text-gray-500"
+        >
           사용자 로그 보기
         </Link>
       </div>
