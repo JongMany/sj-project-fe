52151a1a0fb2aee46b1480bd01a472b9d90557ce
JongMany <blackberry1114@naver.com>
2024-11-10 14:13:57 +0900

    fix: type error로 인한 빌드 오류 수정

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,30 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
-import Link from "next/link";
-import {encodeByAES256} from "@/utils/encode";
+import Link from 'next/link';
+import { encodeByAES256 } from '@/utils/encode';
 
 async function Page() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/user`, {
-    next: {
-      revalidate: 300,
-    }
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/user`,
+    {
+      next: {
+        revalidate: 300,
+      },
+    },
+  );
   const users = await response.json();
 
-
   return (
-      <div className="flex flex-col gap-y-2">
-        {users.map((user, idx) => {
-          const phoneNumber = user.phoneNumber;
-          const id = phoneNumber.slice(5, 9) + idx;
-          const encodedPhoneNumber = encodeByAES256(user.phoneNumber);
-          const encodedId = encodeByAES256(user.id);
-          return <div key={user.id}>
-            <Link href={`/admin/user?id1=${encodedId}&id2=${encodedPhoneNumber}`}>
+    <div className="flex flex-col gap-y-2">
+      {users.map((user: any, idx: number) => {
+        const phoneNumber = user.phoneNumber;
+        const id = phoneNumber.slice(5, 9) + idx;
+        const encodedPhoneNumber = encodeByAES256(user.phoneNumber);
+        const encodedId = encodeByAES256(user.id);
+        return (
+          <div key={user.id}>
+            <Link
+              href={`/admin/user?id1=${encodedId}&id2=${encodedPhoneNumber}`}
+            >
               {id}
             </Link>
           </div>
-        })}
-      </div>
+        );
+      })}
+    </div>
   );
 }
 
--- src/app/admin/user/page.tsx
+++ src/app/admin/user/page.tsx
@@ -1,45 +1,52 @@
 import React from 'react';
-import {decodeByAES256} from "@/utils/encode";
-import Link from "next/link";
+import { decodeByAES256 } from '@/utils/encode';
+import Link from 'next/link';
 
 type Props = {
   searchParams: {
     id1: string;
-    id2: string
-  }
-}
-async function Page({searchParams: {id1 ,id2}}:Props) {
+    id2: string;
+  };
+};
+async function Page({ searchParams: { id1 } }: Props) {
   const encodedId = decodeByAES256(id1);
-  const phoneNumber = decodeByAES256(id2);
-  const slicedId = phoneNumber.slice(5, 9);
+  // const phoneNumber = decodeByAES256(id2);
+  // const slicedId = phoneNumber.slice(5, 9);
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/user/privacy/${encodedId}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL || ''}/api/v1/user/privacy/${encodedId}`,
+  );
   const data = await response.json();
 
   return (
+    <div>
       <div>
-        <div>
-          <div>User 정보</div>
-          <div>ID: {data.id}</div>
-          <div>Group: {data.group}</div>
-        </div>
-        <div>
-          <div>사용 중인 스레드</div>
-          <div className='flex flex-col'>
-            {data?.threads?.length > 0 ? data?.threads?.map((thread) => <div key={thread.threadId} className='flex gap-x-3'>
-              <div className={'min-w-[80px]'}>
-                {thread.type}
-              </div>
-              <Link href={`/admin/${encodedId}/thread?id=${thread.threadId}`}>
-                대화 내용 보기
-              </Link>
-            </div>): <div>사용 중인 스레드가 없습니다</div>}
-          </div>
-          <Link href={`/admin/${encodedId}/logs?id=${encodedId}`}>
-            사용자 로그 보기
-          </Link>
-        </div>
+        <div>User 정보</div>
+        <div>ID: {data.id}</div>
+        <div>Group: {data.group}</div>
       </div>
+      <div>
+        <div>사용 중인 스레드</div>
+        <div className="flex flex-col">
+          {data?.threads?.length > 0 ? (
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            data?.threads?.map((thread: any) => (
+              <div key={thread.threadId} className="flex gap-x-3">
+                <div className={'min-w-[80px]'}>{thread.type}</div>
+                <Link href={`/admin/${encodedId}/thread?id=${thread.threadId}`}>
+                  대화 내용 보기
+                </Link>
+              </div>
+            ))
+          ) : (
+            <div>사용 중인 스레드가 없습니다</div>
+          )}
+        </div>
+        <Link href={`/admin/${encodedId}/logs?id=${encodedId}`}>
+          사용자 로그 보기
+        </Link>
+      </div>
+    </div>
   );
 }
 
